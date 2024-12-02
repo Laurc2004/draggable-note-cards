bdc44ba48c43627e721dddde66295d755a183ea3
Dream <2890808817@qq.com>
2024-12-02 14:27:24 +0800

    feat:完成删除功能

--- src/icons/Trash.tsx
+++ src/icons/Trash.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 interface TrashProps {
   size?: string; // size 是可选的字符串属性，默认值是 "24"
+  cardRef: React.RefObject<HTMLDivElement>;
 }
 
-const Trash: React.FC<TrashProps> = ({ size = "24" }) => {
+const onDeleteNote = (cardRef: React.RefObject<HTMLDivElement>) => {
+  cardRef.current?.remove();
+};
+
+const Trash: React.FC<TrashProps> = ({ size = "24" , cardRef }) => {
   return (
       <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -14,6 +19,7 @@ const Trash: React.FC<TrashProps> = ({ size = "24" }) => {
           stroke="#000000"
           fill="none"
           strokeWidth="1.5"
+          onClick={() => onDeleteNote(cardRef)}
       >
         <path
             d="m6 8 .668 8.681c.148 1.924.222 2.885.84 3.423.068.06.14.115.217.165.685.449 1.63.26 3.522-.118.36-.072.54-.108.721-.111h.064c.182.003.361.039.72.11 1.892.379 2.838.568 3.523.12.076-.05.15-.106.218-.166.617-.538.691-1.5.84-3.423L18 8"
